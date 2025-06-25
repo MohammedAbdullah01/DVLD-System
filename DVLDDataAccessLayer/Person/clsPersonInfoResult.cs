fe68e9072bd56730eb898a396d454ded9ef04673
Mohamed <mm8909840@gmail.com>
2025-06-25 08:19:01 +0300

    Add New Class In DVLDDataAccessLayer/Person (clsPersonInfoResult.cs)

---
+++ DVLDDataAccessLayer/Person/clsPersonInfoResult.cs
@@ -0,0 +1,16 @@
+﻿using DVLDDataAccessLayer.MessageContainer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLDDataAccessLayer.Person
+{
+    public class clsPersonInfoResult
+    {
+        public bool Found { get; set; }
+        public clsPersonInfo Person { get; set; }
+        public Message MessageContainer { get; set; }
+    }
+}
